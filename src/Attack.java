fc3eb8ca61f687f8e2245f517cc6abb88283190e
Simon Bruhse <sbruhse@sbruhse.de>
2017-04-26 09:10:59 +0200

    Simons Bot funktioniert

---
+++ src/Attack.java
@@ -0,0 +1,63 @@
+import bwapi.Position;
+import bwapi.Unit;
+import bwta.BWTA;
+import bwta.BaseLocation;
+
+public class Attack 
+{
+	public static void attack()
+	{
+		Position vPosition = Position.Invalid;
+		for( Unit vUnit : FirstBot.getSelf().getUnits())
+		{
+			if ( vUnit.getPlayer().getID() != FirstBot.getSelf().getID())
+			{
+				vPosition = vUnit.getPosition();
+				break;
+			}
+		}
+		if(vPosition == Position.Invalid)
+		{
+			for(BaseLocation vLocation : BWTA.getStartLocations())
+			{
+				if(!FirstBot.getGame().isExplored(vLocation.getTilePosition()))
+				{
+					vPosition = vLocation.getPosition();
+					break;
+				}
+			}
+		}
+		if(vPosition == Position.Invalid)
+		{
+			for(BaseLocation vLocation : BWTA.getBaseLocations())
+			{
+				if(!FirstBot.getGame().isExplored(vLocation.getTilePosition()))
+				{
+					vPosition = vLocation.getPosition();
+					break;
+				}
+			}
+		}
+		int vCounter = 0;
+		for(Unit vUnit : FirstBot.getSelf().getUnits())
+		{
+			if(vUnit.isIdle() && !vUnit.getType().isWorker() && vUnit.canAttack())
+			{
+				++vCounter;
+			}
+		}
+		
+		if (vCounter < 10)
+		{
+			return;
+		}
+		
+		for(Unit vUnit : FirstBot.getSelf().getUnits())
+		{
+			if(vUnit.isIdle() && !vUnit.getType().isWorker() && vUnit.canAttack() && vPosition != Position.Invalid)
+			{
+				vUnit.attack(vPosition);
+			}
+		}
+	}
+}
